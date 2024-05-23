57e6e5b511adabd1ffef5871100ca67a398a87f6
tomonaganoriaki <tomonaganoriaki@gmail.com>
2024-05-23 12:24:59 +0900

    basic認証の実装完了

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+    matcher: '/:path*'
+};
+
+export default function middleware(req: NextRequest) {
+    const basicAuth = req.headers.get('authorization');
+    if (basicAuth) {
+        const authValue = basicAuth.split(' ')[1];
+        const [user, password] = atob(authValue).split(':');
+
+        if (user === 'user' && password === 'pass') {
+        return NextResponse.next();
+        }
+    }
+
+    return new NextResponse('Unauthorized.', {
+        status: 401,
+        headers: {
+        'WWW-authenticate': 'Basic realm="Secure Area"'
+        }
+    });
+}
