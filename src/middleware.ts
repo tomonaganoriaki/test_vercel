2f0c93ae9d84a2d1919ce7a0d33a90526f22e820
tomonaganoriaki <tomonaganoriaki@gmail.com>
2024-05-23 17:17:45 +0900

    環境によってBasic認証を出しわけ

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server';
 
+const USER = process.env.BASIC_AUTH_USER;
+const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+const ENVIRONMENT = process.env.ENVIRONMENT;
+
 export const config = {
     matcher: '/:path*'
 };
 
 export default function middleware(req: NextRequest) {
+    if (ENVIRONMENT !== 'test') {
+        return NextResponse.next();
+    }
+
     const basicAuth = req.headers.get('authorization');
     if (basicAuth) {
         const authValue = basicAuth.split(' ')[1];
         const [user, password] = atob(authValue).split(':');
 
-        if (user === 'user' && password === 'pass') {
-        return NextResponse.next();
+        if (user === USER && password === PASSWORD) {
+            return NextResponse.next();
         }
     }
 
     return new NextResponse('Unauthorized.', {
         status: 401,
         headers: {
-        'WWW-authenticate': 'Basic realm="Secure Area"'
+            'WWW-authenticate': 'Basic realm="Secure Area"'
         }
     });
 }
